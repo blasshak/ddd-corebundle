d04d305a70d0d223664790768e5b68188377e24c
Blasshak <blasshak@gmail.com>
2017-08-27 15:01:17 -0700

    new arraycollection

---
+++ src/CoreBundle/Infrastructure/Util/Collections/ArrayCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CoreBundle\Infrastructure\Util\Collections;
+
+/**
+ * Class ArrayCollection
+ * @package CoreBundle\Infrastructure\Util\Collections
+ */
+class ArrayCollection extends \Doctrine\Common\Collections\ArrayCollection
+{
+}
